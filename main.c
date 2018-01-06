add85d41247c9d7818d08e096408fcf378a89beb
Antony Pavlov <antonynpavlov@gmail.com>
2018-01-06 23:24:06 +0300

    run memtest on malloc memory pool before initialization
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- main.c
+++ main.c
@@ -8,10 +8,15 @@ extern void mem_malloc_init(void *start, void *end);
 
 extern int rdcycle_cs_init(unsigned int cycle_frequency);
 
+extern int mem_test_moving_inversions(resource_size_t _start, resource_size_t _end);
+
 int main(void);
 
 int __section(.main_entry) constructor(void)
 {
+	mem_test_moving_inversions((void *)MALLOC_BASE,
+			(void *)(MALLOC_BASE + MALLOC_SIZE));
+
 	mem_malloc_init((void *)MALLOC_BASE,
 			(void *)(MALLOC_BASE + MALLOC_SIZE - 1));
 
