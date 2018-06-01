5ccd73e8305811fde5b7f8baaddebbac68f01ba2
Antony Pavlov <antonynpavlov@gmail.com>
2018-06-01 08:25:30 +0300

    import cmdline history code from barebox
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ xfuncs.c
@@ -0,0 +1,52 @@
+/*
+ * xfuncs.c - safe malloc funcions
+ *
+ * based on busybox
+ *
+ * Copyright (c) 2007 Sascha Hauer <s.hauer@pengutronix.de>, Pengutronix
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <malloc.h>
+
+static void enomem_panic(size_t size)
+{
+	printf("out of memory\n");
+	if (size)
+		printf("Unable to allocate %zu bytes\n", size);
+
+	malloc_stats();
+
+	for (;;)
+		;
+}
+
+void *xmalloc(size_t size)
+{
+	void *p = NULL;
+
+	if (!(p = malloc(size)))
+		enomem_panic(size);
+
+	return p;
+}
+EXPORT_SYMBOL(xmalloc);
+
+void *xzalloc(size_t size)
+{
+	void *ptr = xmalloc(size);
+	memset(ptr, 0, size);
+	return ptr;
+}
+EXPORT_SYMBOL(xzalloc);
